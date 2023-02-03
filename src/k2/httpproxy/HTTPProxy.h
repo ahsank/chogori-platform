2ffdc8d9323ea2b0c1d3ca7e16f6a6f413589a09
Ahsan Khan <akhan@futurewei.com>
2023-02-03 11:50:43 -0800

    Snapshot of tatp

--- src/k2/httpproxy/HTTPProxy.h
+++ src/k2/httpproxy/HTTPProxy.h
@@ -96,7 +96,7 @@ private:
 
     std::shared_ptr<shd::Schema> _getSchemaFromCache(const sh::String& cname, std::shared_ptr<dto::Schema> schema);
 
-    void _shdStorageToK2Record(const sh::String& collectionName, shd::SKVRecord::Storage&& key, dto::SKVRecord& k2record);
+    shd::SKVRecord _shdStorageToK2Record(const sh::String& collectionName, shd::SKVRecord::Storage&& key, dto::SKVRecord& k2record);
 
     void _registerAPI();
     void _registerMetrics();
